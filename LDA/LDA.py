fd045cb3b23c38d37c4914721fe828e4aa00e281
Sahil Dadia <dadiasahil94@yahoo.in>
2016-03-14 23:08:41 +0530

    updating my LDA file

--- LDA/LDA.py
+++ LDA/LDA.py
@@ -1,3 +1,4 @@
+from sklearn import lda
 import pylab
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
@@ -15,10 +16,11 @@ def GENERATE(mean_vector , cov_matrix, no_pts=100,class_label=1):
         OUTPUT:
             data : 2D - Numpy matrix of multivariate data
     '''
-    data = np.random.multivariate_normal(class_label ,mean_vector , cov_matrix , no_pts)
-    return data
+    data = np.random.multivariate_normal(mean_vector , cov_matrix , no_pts)
+    class_parameters = (class_label,mean_vector,cov_matrix,no_pts)
+    return data , class_parameters
 
-def GENERATE_RANDOM(no_class=2 , no_features = 2 ,no_pts=100):
+def GENERATE_RANDOM_MULTIVARIATE(no_class=2 , no_features = 2 ,no_pts=100):
     ''' Generates multivariate Gaussian data with random parameters
         INPUT:
             no_class = Number of different classes
@@ -54,23 +56,30 @@ def DISCRIMINANT_FUNCTION(class_parameters):
 
         OUTPUT:
             List of tuple of (classlabel ,A, B ,C) of each class
+            DISCRIMINANT FUNCTION FORMULA
+            g(x) = x'Ax + Bx + C  where x is COLUMN vector and x' = ROW vector
     '''
     no_class = len(class_parameters)
     ABC_list = []
+    total_no_points = sum([class_parameters[i][3] for i in range(len(class_parameters))])*1.0
+
     for i in xrange(no_class):
         # parametes for current class
         mean_vector = np.array(class_parameters[i][1])
         sigma = class_parameters[i][2]
         sig_inv = np.linalg.inv(sigma)
-        current_class_prior = class_parameters[i][3]
+        current_class_prior = class_parameters[i][3]/total_no_points
 
         # DISCRIMINANT FUNCTION FORMULA
         # g(x) = x'Ax + Bx + C  where x is COLUMN vector and x' = ROW vector
+        # A = 2x2 matrix
+        # B = 1x2 matrix
+        # C = 1x1 matrix
 
         A = -0.5*sig_inv
         B = np.dot(sig_inv,mean_vector).T
         C = -0.5*np.dot(np.dot(mean_vector.T,sig_inv),mean_vector) -0.5*np.log(np.linalg.det(sigma))+ np.log(current_class_prior)
-        ABC_list.append((current_class_prior,A,B,C))    #(classlabel ,A, B ,C)
+        ABC_list.append((class_parameters[i][0],A,B,C))    #(classlabel ,A, B ,C)
 
     return ABC_list
 
@@ -84,26 +93,83 @@ def DISCRIMINANT_FUNCTION(class_parameters):
 #
 #         class_parameters.append(( int(i) ,mean , cov , int(no_pts) )) # (label , mean , cov , class priors)
 
+def APPLY_DISCRIMINANT_FUNC(ABC_list , vector):
+    ''' Applies all discriminat function to each vector sample
+        INPUT:
+            vector : row vector of Xbar
+        OUTPUT:
+            A tuple of values given by each function
+    '''
+
+    vector = vector[:,None]  #Add a new axis and make it column vector
+    ans = []
+    for i in range(len(ABC_list)):
+        A,B,C = ABC_list[i][1] , ABC_list[i][2][:,None], ABC_list[i][3]
+        ans.append(float(np.dot(np.dot(vector.T,A),vector) + np.dot(B[:,None].T,vector) + C))
+
+
+    return float(ans.index(min(ans))+1)
+
+def PREDICT_CLASS(funct_output_list):
+    ''' Precidts the class label of the list
+
+    '''
+    pass
+
+
+
 def main():
 
-    # #generate set1
-    # mean1 = np.array([100,120])
-    # cov1 = np.array([[10,0],[0,10]])
-    # data1 = GENERATE(mean1 , cov1)
+    #generate set1
+    mean1 = np.array([100,120])
+    cov1 = np.array([[10,0],[0,10]])
+    data1, class1_para = GENERATE( mean1 , cov1 , class_label=1)
     # for i in range(data1.shape[0]):
     #     print str("0\t") + str(int(data1[i ,0])) + '\t' + str(int(data1[ i ,1]))
-    #
-    # #generate set2
-    # mean1 = np.array([80,180])
-    # cov1 = np.array([[10,0],[0,10]])
-    # data1 = GENERATE(mean1 , cov1)
+
+    #generate set2
+    mean1 = np.array([80,180])
+    cov1 = np.array([[10,0],[0,10]])
+    data2,class2_para = GENERATE(mean1 , cov1,class_label=2)
     # for i in range(data1.shape[0]):
     #     print str("1\t")+ str(int(data1[i ,0])) + '\t' + str(int(data1[ i ,1]))
 
-    data , no_class , no_features , class_parameters = GENERATE_RANDOM(no_class=3 , no_features =3, no_pts=250)
-    hf_LDA.PLOT_3D( data[ : , 0] ,data[ : , 1] , data[ : , 2] )
+    X = np.vstack((data1,data2))
+    y = np.ones_like(X)
+    y[:100]= 1
+    y[100:]= 2
+    y = y[:,0]
+
+    LDAS = lda.LDA()
+    LDAS.fit(X,y)
+    dcf = LDAS.decision_function(X)
+    pred = LDAS.predict(X)
+    print np.unique(pred) , pred.shape
+
+    # print LDAS.coef_, LDAS.intercept_
+    # print dcf[:5]
+    ABC = DISCRIMINANT_FUNCTION((class1_para,class2_para))
+    class_o =[]
+    for vector in X:
+        # print vector
+        class_o.append(APPLY_DISCRIMINANT_FUNC(ABC, vector))
+    class_o = np.array(class_o)
+    # print np.unique(class_o) , np.array(class_o).shape
+    print(class_o[(class_o != pred)])
+
+
+
+    # print LDAS.de
+
+
+    # data , no_class , no_features , class_parameters = GENERATE_RANDOM_MULTIVARIATE(no_class=3 , no_features =3, no_pts=250)
+    # hf_LDA.PLOT_3D( data[ : , 0] ,data[ : , 1] , data[ : , 2] )
     # print data , class_parameters
-    DISCRIMINANT_FUNCTION(class_parameters)
+    # print class1_para
+    # print class2_para
+    # for i in range(len(ABC)):
+    #     for j in range(len(ABC[0])):
+    #         print ABC[i][j]
 
 
 
---
+++ LDA/ReadmeLDA.txt
@@ -0,0 +1,1 @@
+Here the useage of lda is explained
